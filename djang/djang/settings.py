c62702e11f90baeb3916deea04fb6bdb02003b6b
Nitzan Raz <nitz.raz@gmail.com>
2023-01-10 16:21:21 +0200

    Replaced and moved to .env the django secret key

--- djang/djang/settings.py
+++ djang/djang/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)6@vgbw(a%i&xswxi7u476wk@isd$s0#5bu+1r(rz)zeb!z=4#"
+dotenv.load_dotenv() # Allows reading .env files from outside docker
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
