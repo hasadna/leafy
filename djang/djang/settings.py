5dbb307f2a922f752331b324ba7c29cc44eecf73
Ori Hoch <ori@uumpa.com>
2023-01-12 22:26:34 +0200

    allow any host

--- djang/djang/settings.py
+++ djang/djang/settings.py
@@ -32,7 +32,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
